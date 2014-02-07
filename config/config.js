a7ae3b772ab73dd08f4c3b8664eff480bcd8d79c
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-07 11:45:50 +0100

    Remove useless config files for each env

--- config/config.js
+++ config/config.js
@@ -1,11 +1,24 @@
 'use strict';
 
-// Utilize Lo-Dash utility library
-var _ = require('lodash');
+var path = require('path');
+var rootPath = path.normalize(__dirname + '/..');
+
 
 // Extend the base configuration in all.js with environment
 // specific configuration
-module.exports = _.extend(
-    require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.js') || {}
-);
+module.exports = {
+    root: rootPath,
+    port: process.env.PORT || 3000,
+    db: process.env.MONGOHQ_URL || 'mongodb://localhost/growstuff-test',
+
+    sessionSecret: 'GrOwStuFf',
+    sessionCollection: 'sessions',
+
+    app: { name: 'GrowStuff' },
+
+    twitter: {
+        clientID: process.env.TWITTER_CONSUMER_KEY,
+        clientSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL: process.env.TWITTER_CALLBACK_URL || 'http://localhost:3000/auth/twitter/callback'
+    }
+};

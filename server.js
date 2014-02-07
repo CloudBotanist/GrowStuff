c7ef82b7069c7e733aee0cd2e3f8eda48152f5a8
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-07 17:58:44 +0100

    Retweet if not understood

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var express = require('express'),
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Initializing system variables
+
 var config = require('./config/config'),
     mongoose = require('mongoose');
 

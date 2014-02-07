cecbbc2dd64b48165e999aa7cca019d5a800dbcf
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-07 10:18:09 +0100

    Update for port chacking

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ sio.sockets.on('connection', function (socket) {
     });
 });
 
-server.listen(3000);
+server.listen(port);
 console.log('Express app started on port ' + port);
 
 // Initializing logger

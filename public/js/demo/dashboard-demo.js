487c0d2be26796966b6e2492091b72557c266eb3
Mohammed Elalj <mohammed@elalj.com>
2014-02-21 11:07:37 +0100

    new branding

--- public/js/demo/dashboard-demo.js
+++ public/js/demo/dashboard-demo.js
@@ -1,103 +1,3 @@
-//Date Range Picker
-$(document).ready(function() {
-    $('#reportrange').daterangepicker({
-            startDate: moment().subtract('days', 29),
-            endDate: moment(),
-            minDate: '01/01/2012',
-            maxDate: '12/31/2014',
-            dateLimit: {
-                days: 60
-            },
-            showDropdowns: true,
-            showWeekNumbers: true,
-            timePicker: false,
-            timePickerIncrement: 1,
-            timePicker12Hour: true,
-            ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                'Last 7 Days': [moment().subtract('days', 6), moment()],
-                'Last 30 Days': [moment().subtract('days', 29), moment()],
-                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-            },
-            opens: 'left',
-            buttonClasses: ['btn btn-default'],
-            applyClass: 'btn-small btn-primary',
-            cancelClass: 'btn-small',
-            format: 'MM/DD/YYYY',
-            separator: ' to ',
-            locale: {
-                applyLabel: 'Submit',
-                fromLabel: 'From',
-                toLabel: 'To',
-                customRangeLabel: 'Custom Range',
-                daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                firstDay: 1
-            }
-        },
-        function(start, end) {
-            console.log("Callback has been called!");
-            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-        }
-    );
-    //Set the initial state of the picker label
-    $('#reportrange span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
-});
-
-//Morris Area Chart
-var sales_data = [{
-    date: '2014-1-25',
-    productA: 80.26
-}, {
-    date: '2014-1-26',
-    productA: 251.34
-}, {
-    date: '2014-1-27',
-    productA: 90.91
-}, {
-    date: '2014-1-28',
-    productA: 91.23
-}, {
-    date: '2014-1-29',
-    productA: 148.26
-}, {
-    date: '2014-1-30',
-    productA: 74.53
-}, {
-    date: '2014-1-31',
-    productA: 84.26
-}, ];
-Morris.Area({
-    element: 'morris-chart-dashboard',
-    data: sales_data,
-    xkey: 'date',
-    xLabelFormat: function(date) {
-        return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-    },
-    xLabels: 'day',
-    ykeys: ['productA'],
-    yLabelFormat: function(y) {
-        return y+"ml";
-    },
-    labels: ['Product A'],
-    lineColors: ['#fff'],
-    hideHover: 'auto',
-    resize: true,
-    gridTextFamily: ['Open Sans'],
-    gridTextColor: ['rgba(255,255,255,0.7)'],
-    fillOpacity: 0.1,
-    pointSize: 0,
-    smooth: true,
-    lineWidth: 2,
-    grid: true,
-    dateFormat: function(date) {
-        d = new Date(date);
-        return (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
-    },
-});
-
 
 
 //Chat Widget SlimScroll Box
@@ -131,8 +31,8 @@ $(document).ready(function() {
 
 
 //Easy Pie Charts
-$(function() {
-    $('#easy-pie-1, #easy-pie-2, #easy-pie-3, #easy-pie-4').easyPieChart({
+$(document).ready(function() {
+    $('#easy-pie-1').easyPieChart({
         barColor: "rgba(255,255,255,.5)",
         trackColor: "rgba(255,255,255,.5)",
         scaleColor: "rgba(255,255,255,.5)",
@@ -144,4 +44,40 @@ $(function() {
         }
     });
 
+    //Morris Area Chart
+    var sales_data = new Array();
+    for(i=0; i < plantStatus.length; i++){
+        sales_data[i] = {
+            date: plantStatus[i].created.split(".")[0].replace("T"," "),
+            hum: plantStatus[i].hum};
+    }
+    
+    Morris.Area({
+        element: 'morris-chart-dashboard',
+        data: sales_data,
+        xkey: 'date',
+        xLabelFormat: function(date) {
+            return (d.getHours() + ":"+ d.getMinutes() + ":" +d.getSeconds());
+        },
+        xLabels: 'day',
+        ykeys: ['hum',],
+        yLabelFormat: function(y) {
+            return y;
+        },
+        labels: ['Humidity'],
+        lineColors: ['#fff'],
+        hideHover: 'auto',
+        resize: true,
+        gridTextFamily: ['Open Sans'],
+        gridTextColor: ['rgba(255,255,255,0.7)'],
+        fillOpacity: 0.1,
+        pointSize: 0,
+        smooth: true,
+        lineWidth: 2,
+        grid: true,
+        dateFormat: function(date) {
+            d = new Date(date);
+            return (d.getHours() + ":"+ d.getMinutes() + ":" +d.getSeconds());
+        }
+    });
 });

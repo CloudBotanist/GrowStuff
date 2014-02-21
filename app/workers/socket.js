902c8d153d218c95350c8b89674d5e57979061fc
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-21 06:33:17 +0100

    Move socket back on Id

--- app/workers/socket.js
+++ app/workers/socket.js
@@ -36,7 +36,7 @@ module.exports.init = function(sio) {
     sio.sockets.on('connection', function (socket) {
 
         socket.on('identification', function (data) {
-            Plant.findOne({mac_adress: data.id}, function(err, plant) {
+            Plant.findOne({_id: data.id}, function(err, plant) {
                 if (!err && plant) {
                     console.log('New connection from :' + plant.name);
 
